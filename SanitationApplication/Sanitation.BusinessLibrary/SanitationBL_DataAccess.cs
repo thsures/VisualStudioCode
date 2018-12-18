41a84e67a96605c8b571f26409518e9c36b306cb
Suresh Thippani <Suresh.Thippani@brandmuscle.com>
2018-12-18 16:00:48 +0530

    My Test Code

---
+++ SanitationApplication/Sanitation.BusinessLibrary/SanitationBL_DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sanitation.BusinessServices;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sanitation.BusinessLibrary
+{
+    public class SanitationBL_DataAccess
+    {
+        Sanitation.DataModel.SanitationEntities _db = new DataModel.SanitationEntities(); 
+        public string Insert(string username,string password)
+        {
+            UserRegistration objuserregistration = new UserRegistration();
+            objuserregistration.EmailID = username;
+            objuserregistration.PhoneNumber = "9177536619";
+            objuserregistration.UserID = 1;
+            objuserregistration.RoleID = 1;
+            objuserregistration.AddressOne = "Hyd";
+            objuserregistration.AddressTwo = "Ban";
+            _db.UserRegistrations.Add(objuserregistration);
+            return username;
+
+        }
+    }
+}
---
+++ SanitationApplication/Sanitation.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Sanitation.WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sanitation.BusinessLibrary;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sanitation.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel usermodel)
+        {
+            SanitationBL_DataAccess blaccess = new SanitationBL_DataAccess();
+            string value = blaccess.Insert(usermodel.UserName, usermodel.Password);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
